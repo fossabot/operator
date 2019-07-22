40ef4204dbfb7b78892c2d9e200fa4862f7a4c7e
Tamal Saha <tamal@appscode.com>
2019-07-21 18:17:14 -0700

    Fix dev deployment script for operator
    Fix: https://github.com/kubedb/project/issues/597

--- hack/deploy/setup.sh
+++ hack/deploy/setup.sh
@@ -9,10 +9,66 @@ export MINIKUBE_RUN=0
 export SELF_HOSTED=1
 export ARGS="" # Forward arguments to installer script
 
-REPO_ROOT="$GOPATH/src/github.com/kubedb/operator"
-CLI_ROOT="$GOPATH/src/github.com/kubedb/cli"
+REPO_ROOT=`git rev-parse --show-toplevel`
+INSTALLER_ROOT="$GOPATH/src/github.com/kubedb/installer"
 
 pushd $REPO_ROOT
+
+OS=""
+ARCH=""
+DOWNLOAD_URL=""
+DOWNLOAD_DIR=""
+TEMP_DIRS=()
+ONESSL=""
+
+# http://redsymbol.net/articles/bash-exit-traps/
+function cleanup() {
+  rm -rf ca.crt ca.key server.crt server.key
+  # remove temporary directories
+  for dir in "${TEMP_DIRS[@]}"; do
+    rm -rf "${dir}"
+  done
+}
+trap cleanup EXIT
+
+# detect operating system
+# ref: https://raw.githubusercontent.com/helm/helm/master/scripts/get
+function detectOS() {
+  OS=$(echo `uname`|tr '[:upper:]' '[:lower:]')
+
+  case "$OS" in
+    # Minimalist GNU for Windows
+    cygwin* | mingw* | msys*) OS='windows';;
+  esac
+}
+
+# detect machine architecture
+function detectArch() {
+  ARCH=$(uname -m)
+  case $ARCH in
+    armv7*) ARCH="arm";;
+    aarch64) ARCH="arm64";;
+    x86) ARCH="386";;
+    x86_64) ARCH="amd64";;
+    i686) ARCH="386";;
+    i386) ARCH="386";;
+  esac
+}
+
+detectOS
+detectArch
+
+# download file pointed by DOWNLOAD_URL variable
+# store download file to the directory pointed by DOWNLOAD_DIR variable
+# you have to sent the output file name as argument. i.e. downloadFile myfile.tar.gz
+function downloadFile() {
+  if curl --output /dev/null --silent --head --fail "$DOWNLOAD_URL"; then
+    curl -fsSL ${DOWNLOAD_URL} -o $DOWNLOAD_DIR/$1
+  else
+    echo "File does not exist"
+    exit 1
+  fi
+}
 
 onessl_found() {
   # https://stackoverflow.com/a/677212/244009
@@ -28,37 +84,26 @@ onessl_found() {
   return 1
 }
 
+# download onessl if it does not exist
 onessl_found || {
   echo "Downloading onessl ..."
-  if [[ "$(uname -m)" == "aarch64" ]]; then
-    curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-linux-arm64
-    chmod +x onessl
-    export ONESSL=./onessl
-  else
-    # ref: https://stackoverflow.com/a/27776822/244009
-    case "$(uname -s)" in
-      Darwin)
-        curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-darwin-amd64
-        chmod +x onessl
-        export ONESSL=./onessl
-        ;;
 
-      Linux)
-        curl -fsSL -o onessl https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-linux-amd64
-        chmod +x onessl
-        export ONESSL=./onessl
-        ;;
+  ARTIFACT="https://github.com/kubepack/onessl/releases/download/0.12.0"
+  ONESSL_BIN=onessl-${OS}-${ARCH}
+  case "$OS" in
+    cygwin* | mingw* | msys*)
+      ONESSL_BIN=${ONESSL_BIN}.exe
+    ;;
+  esac
 
-      CYGWIN* | MINGW* | MSYS*)
-        curl -fsSL -o onessl.exe https://github.com/kubepack/onessl/releases/download/0.10.0/onessl-windows-amd64.exe
-        chmod +x onessl.exe
-        export ONESSL=./onessl.exe
-        ;;
-      *)
-        echo 'other OS'
-        ;;
-    esac
-  fi
+  DOWNLOAD_URL=${ARTIFACT}/${ONESSL_BIN}
+  DOWNLOAD_DIR="$(mktemp -dt onessl-XXXXXX)"
+  TEMP_DIRS+=($DOWNLOAD_DIR) # store DOWNLOAD_DIR to cleanup later
+
+  downloadFile $ONESSL_BIN # downloaded file name will be saved as the value of ONESSL_BIN variable
+
+  export ONESSL=${DOWNLOAD_DIR}/${ONESSL_BIN}
+  chmod +x $ONESSL
 }
 
 source "$REPO_ROOT/hack/deploy/settings"
@@ -66,7 +111,7 @@ source "$REPO_ROOT/hack/libbuild/common/lib.sh"
 
 export KUBE_CA=$($ONESSL get kube-ca | $ONESSL base64)
 export APPSCODE_ENV=${APPSCODE_ENV:-prod}
-export KUBEDB_SCRIPT="curl -fsSL https://raw.githubusercontent.com/kubedb/cli/0.9.0-beta.0/"
+export KUBEDB_SCRIPT="curl -fsSL https://raw.githubusercontent.com/kubedb/installer/$INSTALLER_BRANCH/"
 
 show_help() {
   echo "setup.sh - setup kubedb operator"
@@ -118,22 +163,22 @@ done
 # Update "CLI_BRANCH" in deploy/settings file to pull a particular CLI repo branch.
 if [ "$APPSCODE_ENV" = "dev" ]; then
   detect_tag ''
-  export KUBEDB_SCRIPT="cat $CLI_ROOT/"
+  export KUBEDB_SCRIPT="cat $INSTALLER_ROOT/"
   export CUSTOM_OPERATOR_TAG=$TAG
   echo ""
 
-  if [[ ! -d $CLI_ROOT ]]; then
+  if [[ ! -d $INSTALLER_ROOT ]]; then
     echo ">>> Cloning cli repo"
-    git clone -b $CLI_BRANCH https://github.com/kubedb/cli.git "${CLI_ROOT}"
-    pushd $CLI_ROOT
+    git clone -b $INSTALLER_BRANCH https://github.com/kubedb/installer.git "${INSTALLER_ROOT}"
+    pushd $INSTALLER_ROOT
   else
-    pushd $CLI_ROOT
+    pushd $INSTALLER_ROOT
     detect_tag ''
-    if [[ $git_branch != $CLI_BRANCH ]]; then
+    if [[ $git_branch != $INSTALLER_BRANCH ]]; then
       git fetch --all
-      git checkout $CLI_BRANCH
+      git checkout $INSTALLER_BRANCH
     fi
-    git pull --ff-only origin $CLI_BRANCH #Pull update from remote only if there will be no conflict.
+    git pull --ff-only origin $INSTALLER_BRANCH #Pull update from remote only if there will be no conflict.
   fi
 fi
 
@@ -142,31 +187,30 @@ env | sort | grep -e KUBEDB* -e APPSCODE*
 echo ""
 
 if [ "$SELF_HOSTED" -eq 1 ]; then
-  echo "${KUBEDB_SCRIPT}hack/deploy/kubedb.sh | bash -s -- $ARGS"
-  ${KUBEDB_SCRIPT}hack/deploy/kubedb.sh | bash -s -- ${ARGS}
+  echo "${KUBEDB_SCRIPT}deploy/kubedb.sh | bash -s -- $ARGS"
+  ${KUBEDB_SCRIPT}deploy/kubedb.sh | bash -s -- ${ARGS}
 fi
 
 if [ "$MINIKUBE" -eq 1 ]; then
-  cat $CLI_ROOT/hack/deploy/validating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
-  cat $CLI_ROOT/hack/deploy/mutating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
+  cat $INSTALLER_ROOT/deploy/validating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
+  cat $INSTALLER_ROOT/deploy/mutating-webhook.yaml | $ONESSL envsubst | kubectl apply -f -
   cat $REPO_ROOT/hack/dev/apiregistration.yaml | $ONESSL envsubst | kubectl apply -f -
-  # Following line may give error if DBVersions CRD already not created
-  cat $CLI_ROOT/hack/deploy/kubedb-catalog/* | $ONESSL envsubst | kubectl apply -f - || true
 
   if [ "$MINIKUBE_RUN" -eq 1 ]; then
-    $REPO_ROOT/hack/make.py
-    kubedb-operator run --v=3 \
+    pushd $REPO_ROOT
+    make
+    ./bin/${OS}_${ARCH}/operator run --v=3 \
       --secure-port=8443 \
       --enable-status-subresource=true \
       --enable-mutating-webhook=true \
       --enable-validating-webhook=true \
       --kubeconfig="$HOME/.kube/config" \
       --authorization-kubeconfig="$HOME/.kube/config" \
-      --authentication-kubeconfig="$HOME/.kube/config"
+      --authentication-kubeconfig="$HOME/.kube/config"  
   fi
 fi
 
-if [ $(pwd) = "$CLI_ROOT" ]; then
+if [ $(pwd) = "$INSTALLER_ROOT" ]; then
   popd
 fi
 popd
